94ba09b2c05a6ddbffce30206bb446ee87b762c4
ALI_Zucker <zabihiali1998@gmail.com>
2024-03-31 14:23:21 +0330

    create header

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      textColor:{
+        "semiDarkPer":"#4c4294",
+        "lightPer":"#46039a",
+        "titlePer":"#412f59",
+        "lowPre":"#7a65a5"
+      },
+      backgroundColor:{
+        "semiDarkPer":"#4c4294",
+        "lightPer":"#46039a",
+        "titlePer":"#412f59",
+        "lowPre":"#7a65a5"
+      }
+    },
+  },
+  plugins: [],
+}
